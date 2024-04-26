e22ffc90b34e975e9c50bb3523e8aec34a20edb4
muohannedxd <mohanned.kadache@ensia.edu.dz>
2024-04-27 00:56:55 +0100

    map added

--- lib/screens/Map.dart
+++ lib/screens/Map.dart
@@ -1,8 +1,8 @@
-// ignore_for_file: prefer_const_constructors, avoid_print, sort_child_properties_last
-
+// ignore_for_file: prefer_const_constructors, avoid_print, sort_child_properties_last, deprecated_member_use
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
 import 'package:navira/constants/custom_colors.dart';
-import 'package:navira/constants/text_sizes.dart';
+import 'package:latlong2/latlong.dart'; // Import LatLng from the correct package
 
 class Settings extends StatelessWidget {
   const Settings({super.key});
@@ -10,10 +10,19 @@ class Settings extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      backgroundColor: CustomColors.bgColor,
-      body: Center(child: Text('Settings'),)
-    );
+        backgroundColor: CustomColors.bgColor,
+        body: Center(
+            child: FlutterMap(
+                options: MapOptions(
+                  center: LatLng(36.752294, 5.090494), 
+                  zoom: 15,
+                ),
+                children: [
+              TileLayer(
+                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                userAgentPackageName: 'com.example.app',
+              ),
+            ])));
   }
 }
